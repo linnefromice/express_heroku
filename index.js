0193a2b55e41d855c0feadbe74bc7fd61a022a3b
linnefromice <linnefromice@gmail.com>
2020-02-21 22:46:36 +0900

    add express-partials

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const partials = require('express-partials');
 const app = express();
 app.set('view engine', 'ejs');
+app.use(partials())
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.render('home', { creator: "linnefromice"});
+  response.render('home', { title: "Home"});
 });
 
 const port = app.get('port');

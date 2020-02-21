d4d33f1125a31253dc7508b0e861ffe9bb42a19b
linnefromice <linnefromice@gmail.com>
2020-02-21 21:48:51 +0900

    add ejs & add simple page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello world! created by linnefromice');
+  response.render('home', { creator: "linnefromice"});
 });
 
 const port = app.get('port');

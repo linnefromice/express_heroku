6b799560e2352872f1c4a5b2fae6f17287e14cb5
linnefromice <linnefromice@gmail.com>
2020-02-23 00:22:33 +0900

    add router - separate router for pages

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const partials = require('express-partials');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const pageRouter = require('./routes/page');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -12,9 +13,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(request, response) {
-  response.render('home', { title: "Home"});
-});
+app.use('', pageRouter);
 
 app.get('/api/movie', (request, response) => {
   fs.readFile('./data/movie.json', 'utf8', (err, data) => {

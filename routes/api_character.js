c9c120764afe44855f6d413900eaa0e4af203abe
linnefromice <linnefromice@gmail.com>
2020-02-29 15:44:20 +0900

    use repository class

--- routes/api_character.js
+++ routes/api_character.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
+const repository = require('../repository/FileRepository');
 
 router.get('/', (request, response) => {
-  const data = fs.readFileSync('./data/character.json', 'utf8');
+  const data = repository.getCharacters();
   response.send(JSON.parse(data));
 })
 
 router.get('/:id', (request, response) => {
   const selectedId = request.params.id;
 
-  const data = fs.readFileSync('./data/character.json', 'utf8');
+  const data = repository.getCharacters();
 
   const jsonData = JSON.parse(data)
   for (let dto of jsonData) {
--- routes/api_movie.js
+++ routes/api_movie.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
+const repository = require('../repository/FileRepository');
 
 router.get('/', (request, response) => {
-  const data = fs.readFileSync('./data/movie.json', 'utf8');
+  const data = repository.getMovies();
   response.send(JSON.parse(data));
 });
 
 router.get('/:id', (request, response) => {
   const selectedId = request.params.id;
 
-  const data = fs.readFileSync('./data/movie.json', 'utf8');
+  const data = repository.getMovies();
   
   const jsonData = JSON.parse(data)
   for (let dto of jsonData) {

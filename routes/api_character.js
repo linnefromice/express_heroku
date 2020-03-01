3f945192e6995df88642dd171ea586aa7d22127c
linnefromice <linnefromice@gmail.com>
2020-03-01 19:26:22 +0900

    refactor - remove unnecessary processing

--- routes/api_character.js
+++ routes/api_character.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Service = require('../service/character_service');
-const Character = require('../model/character');
 
 router.get('/', (request, response) => {
   const dtoList = Service.selectAll();

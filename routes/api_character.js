2ac45c65138f7b7f02b77b28d2dee4a89d91ee0a
linnefromice <linnefromice@gmail.com>
2020-03-01 16:11:44 +0900

    refactor - use model class & add conversion process

--- routes/api_character.js
+++ routes/api_character.js
@@ -1,25 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const repository = require('../repository/FileRepository');
+const Character = require('../models/Character');
 
 router.get('/', (request, response) => {
   const data = repository.getCharacters();
-  response.send(JSON.parse(data));
+  const jsonList = JSON.parse(data);
+
+  const dtoList = [];
+  for (let json of jsonList) {
+    dtoList.push(new Character(json.id, json.name, json.joinedTime));
+  }
+
+  response.send(dtoList)
 })
 
 router.get('/:id', (request, response) => {
   const selectedId = request.params.id;
 
   const data = repository.getCharacters();
+  const jsonList = JSON.parse(data);
 
-  const jsonData = JSON.parse(data)
-  for (let dto of jsonData) {
+  const dtoList = [];
+  for (let json of jsonList) {
+    dtoList.push(new Character(json.id, json.name, json.joinedTime));
+  }
+
+  for (let dto of dtoList) {
     if (selectedId == dto.id) {
-      response.send(dto)
+      response.send(dto);
       return
     }
   }
-  response.send(jsonData)
+  response.send(dtoList)
 })
 
 module.exports = router;

37ef172d094124e11d66096f1bcc58bed66487c3
linnefromice <linnefromice@gmail.com>
2020-03-05 11:52:25 +0900

    add new pages for characters/movies (sample logic)

--- routes/page.js
+++ routes/page.js
@@ -5,5 +5,13 @@ router.get('/', function(request, response) {
   response.type('html')
   response.status(200).render('home', { title: "Home"});
 });
+router.get('/characters', function(request, response) {
+  response.type('html')
+  response.status(200).render('characters_page');
+});
+router.get('/movies', function(request, response) {
+  response.type('html')
+  response.status(200).render('movies_page');
+});
 
 module.exports = router;

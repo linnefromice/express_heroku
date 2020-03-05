21535f7b7d5fbf9cb537ee5412a0641328291986
linnefromice <linnefromice@gmail.com>
2020-03-05 12:00:15 +0900

    modify layout for list page (display json data)

--- routes/page.js
+++ routes/page.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const characterService = require('../service/character_service');
+const movieService = require('../service/movie_service');
 
 router.get('/', function(request, response) {
   response.type('html')
-  response.status(200).render('home', { title: "Home"});
+  response.status(200).render('home', { title: "Home" });
 });
 router.get('/characters', function(request, response) {
   response.type('html')
-  response.status(200).render('characters_page');
+  response.status(200).render('characters_page', { list: characterService.selectAll() });
 });
 router.get('/movies', function(request, response) {
   response.type('html')
-  response.status(200).render('movies_page');
+  response.status(200).render('movies_page', { list: movieService.selectAll() });
 });
 
 module.exports = router;

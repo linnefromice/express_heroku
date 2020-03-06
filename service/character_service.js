ba19c0ffcab0dd721ed95f0859e99f868d60d5bc
linnefromice <linnefromice@gmail.com>
2020-03-07 01:34:54 +0900

    add service logic (get one dto)

--- service/character_service.js
+++ service/character_service.js
@@ -6,4 +6,14 @@ module.exports = class CharacterService {
   static selectAll() {
     return repository.getCharacters();
   }
+
+  static selectOne(id) {
+    const dtoList = this.selectAll();
+    for (let dto of dtoList) {
+      if (selectedId == dto.id) {
+        return dto;
+      }
+    }
+    return null;
+  }
 }
--- service/movie_service.js
+++ service/movie_service.js
@@ -6,4 +6,14 @@ module.exports = class MovieService {
   static selectAll() {
     return repository.getMovies();
   }
+
+  static selectOne(id) {
+    const dtoList = this.selectAll();
+    for (let dto of dtoList) {
+      if (selectedId == dto.id) {
+        return dto;
+      }
+    }
+    return null;
+  }
 }

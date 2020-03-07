c7920923aa3067cb6348130650d1d6c68a1aceb0
linnefromice <linnefromice@gmail.com>
2020-03-07 15:17:39 +0900

    modify service test (class name)

--- __tests__/service/character_service.js
+++ __tests__/service/character_service.js
@@ -1,6 +1,10 @@
 const service = require('../../service/character_service');
+const Character = require('../../model/character');
 
 test('Test_CharacterService_selectAll_01', () => {
   const result = service.selectAll();
-  expect(Array.isArray(result)).toBe(true); 
+  expect(Array.isArray(result)).toBe(true);
+  for (let dto of result) {
+    expect(dto instanceof Character).toBe(true)
+  }
 })
--- __tests__/service/movie_service.js
+++ __tests__/service/movie_service.js
@@ -1,6 +1,11 @@
 const service = require('../../service/movie_service');
+const Movie = require('../../model/Movie');
 
 test('Test_MovieService_selectAll_01', () => {
   const result = service.selectAll();
-  expect(Array.isArray(result)).toBe(true); 
+  expect(Array.isArray(result)).toBe(true);
+  for (let dto of result) {
+    // expect(dto instanceof Movie).toBe(true)
+    expect(dto.constructor.name).toBe(Movie.name)
+  }
 })
